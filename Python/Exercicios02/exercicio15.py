ceb30b6930e55dffa9cc960b67e7d6511319daff
joaoresende89 <joaopsmr@gmail.com>
2023-10-05 16:49:00 -0300

    init

--- Python/Exercicios02/exercicio15.py
+++ Python/Exercicios02/exercicio15.py
@@ -1,5 +1,19 @@
 # A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até o n−ésimo termo.
 
+num = int(input("Informe quantos termos você quer ver da sequincia Fibonacci: "))
 
+if num < 1:
+    exit()
+
+fib = list(range(num))
+
+for i in range(0,num):
+    if i < 2:
+        fib[i] = 1
+    else:
+       fib[i] = fib[i-2] + fib[i-1]
+    print(fib[i])
+
+    
 
 
---
+++ Python/Exercicios02/exercicio16.py
@@ -0,0 +1,15 @@
+# A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,... Faça um programa que gere a série até que o valor seja maior que 500.
+
+fib = list()
+i = 0
+print(fib[0])
+
+while fib[i] < 500:
+    if i < 2:
+        fib[i] = 1
+    else:
+       fib[i] = fib[i-2] + fib[i-1]
+    print(fib[i])
+    i += 1
+    
+
