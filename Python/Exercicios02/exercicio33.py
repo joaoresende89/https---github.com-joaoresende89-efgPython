2c5c4fea98b331013349b988c7a5fc62c45bff53
joaoresende89 <joaopsmr@gmail.com>
2023-10-16 15:18:13 -0300

    .

--- Python/Exercicios02/exercicio33.py
+++ Python/Exercicios02/exercicio33.py
@@ -1,12 +1,25 @@
 # O Departamento Estadual de Meteorologia lhe contratou para desenvolver um programa que leia as um conjunto indeterminado de temperaturas,
 # e informe ao final a menor e a maior temperaturas informadas, bem como a média das temperaturas.
 
-temp = input("Informe as temperaturas separadas por vígula: ")
+# temp = input("Informe as temperaturas separadas por vígula: ")
+i = 0
+temp = []
 
-teste = temp.split(", ")
+while True:
+    temp.append(input("Informe a temperatura: "))
+    if temp[i] != '':
+        i += 1
+    else:
+        break
+    
+print(f"A temperatura máxima é: {max(temp[:-1])}")
+print(f"A temperatura mínima é: {min(temp[:-1])}")
+newTemp = [float(string) for string in temp[:-1]]
+print(f"A média de temperatura é: {(sum(newTemp))/i:.1f}")
 
-print(sum(teste))
+# teste = temp.split(", ")
+
+# print(temp)    
 
 
 
-# while
